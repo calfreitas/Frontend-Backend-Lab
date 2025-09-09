cf89dbc43ddbded8748e4dc028beb1884a979b50
Calliel <callieldefreitas@gmail.com>
2025-09-09 14:42:59 -0300

    first commit

---
+++ React/F01/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class ='container'>
+        <div class='sq1red'>1</div>  
+        <div class='sq2blue'>2</div>  
+        <div class='sq3green'>3</div>  
+        <div class='sq4yellow'>4</div>  
+      </div>              
+    </>
+  )
+}
+
+export default App
