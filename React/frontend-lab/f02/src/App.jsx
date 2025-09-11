376eae08a7a378ba604999ec2e8632ef4a0a4a4e
Calliel <callieldefreitas@gmail.com>
2025-09-11 15:56:24 -0300

    add: nova atividade f02 - construcao de profileCard Component - css + html e conceitos de componentizacao + hooks

---
+++ React/frontend-lab/f02/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ProfileCardComponent from './components/profileCard'
+
+function App() {
+
+  return (
+    <>
+      <div className="MainContainer">
+        <div className="SubContainerProfileCard">
+          <ProfileCardComponent />
+        </div>
+      </div>              
+    </>
+  )
+}
+
+export default App
